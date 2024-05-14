99bffbfaae0f47b8a2272f3ae421b7a038af5595
Parkjyun <jaeyun0507@naver.com>
2024-05-14 22:59:25 +0900

    [init] initial settings

---
+++ src/test/java/com/spotify/spotifyserver/SpotifyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify.spotifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
